f27adf68363bf06f37da66d60f24bb2b71b5426f
Paweł Farys <pmg.farys@gmail.com>
2022-07-23 18:08:24 +0200

    Moved key to env

--- dotfiles/zsh/config.d/functions/utils/iploc.zsh
+++ dotfiles/zsh/config.d/functions/utils/iploc.zsh
@@ -2,9 +2,8 @@ iploc() {
   IP=$1
 
   if [ -x "$(command -v http)" ]; then
-    http -p b GET "https://geo.ipify.org/api/v1?apiKey=at_wwTbqYjp1e9hWg8FEz37HZRUKR6Mu&ipAddress=${IP}"
+    http -p b GET "https://geo.ipify.org/api/v1?apiKey=${IPIFY_API_KEY}&ipAddress=${IP}"
   else
-    curl "https://geo.ipify.org/api/v1?apiKey=at_wwTbqYjp1e9hWg8FEz37HZRUKR6Mu&ipAddress=${IP}" | json_pp
+    curl "https://geo.ipify.org/api/v1?apiKey=${IPIFY_API_KEY}&ipAddress=${IP}" | json_pp
   fi
-
 }
