1626a57b6e86c832df576b24a3381bf98a83627b
Paweł Farys <pmg.farys@gmail.com>
2023-01-06 17:31:27 +0100

    Fixed idle script

--- dotfiles/sway-env/sway/bin/idle.sh
+++ dotfiles/sway-env/sway/bin/idle.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-swayidle -w \
-  timeout 300 '/home/vertisan/.config/sway/bin/lockscreen.sh' \
-  timeout 360 'swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"' \
-  before-sleep '/home/vertisan/.config/sway/bin/lockscreen.sh'
+swayidle -w timeout 300 '/home/vertisan/.config/sway/bin/lockscreen.sh' timeout 360 'swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"' before-sleep '/home/vertisan/.config/sway/bin/lockscreen.sh'
+
