78995af41469e5ace8781b91fe3fe6361413f3f9
Paweł Farys <pmg.farys@gmail.com>
2024-03-29 00:46:08 +0100

    GPG things

---
+++ zsh/config.d/functions/gpg/encryption.zsh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+secret () {
+  output=~/"${1}".$(date +%s).enc
+  gpg --encrypt --armor --output ${output} \
+    -r $KEYID "${1}" && echo "${1} -> ${output}"
+}
+
+reveal () {
+  output=$(echo "${1}" | rev | cut -c16- | rev)
+  gpg --decrypt --output ${output} "${1}" && \
+    echo "${1} -> ${output}"
+}
